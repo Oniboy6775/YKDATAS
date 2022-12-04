54d64c80a2270cdfb3c606b3f3a4bbafd521b485
ONIBOY <onisabiabdullahi@gmail.com>
2022-12-04 12:52:28 +0100

    unwanted endpoint removed

--- Controllers/purchaseControllers.js
+++ Controllers/purchaseControllers.js
@@ -1,13 +1,7 @@
 const User = require("../Models/usersModel");
-const {
-  AIRTIME_RECEIPT,
-  DATA_RECEIPT,
-  ELECETRICITY_RECEIPT,
-} = require("./TransactionReceipt");
+const { AIRTIME_RECEIPT, DATA_RECEIPT } = require("./TransactionReceipt");
 const Data = require("../Models/dataModel");
-const { disco } = require("../API_DATA/disco");
 
-const axios = require("axios");
 const BUYAIRTIME = require("./APICALLS/Airtime/buyAirtime");
 const BUYELECTRICITY = require("./APICALLS/Electricity/buyElectricity");
 const BUYDATA = require("./APICALLS/Data/Data");
@@ -117,95 +111,24 @@ const buyData = async (req, res) => {
 };
 
 const validateMeter = async (req, res) => {
-  const { meterNumber, meterId, meterType } = req.body;
-  if (!meterNumber && !meterId)
-    return res.status(400).json({ msg: "All fields are required" });
-  const meterIdObj = disco.find((e) => e.id == meterId);
-  if (!meterIdObj) return res.status(400).json({ msg: "Invalid meter ID" });
-  const { id, name } = meterIdObj;
-  // console.log(req.body);
-  try {
-    const ValidateMeterResponse = await axios.get(
-      `https://www.gladtidingsdata.com/ajax/validate_meter_number/?meternumber=${meterNumber}&disconame=${name}&mtype=${meterType.toUpperCase()}`,
-      {
-        headers: {
-          Authorization: process.env.GLADITINGSDATA_TOKEN,
-        },
-      }
-    );
-    if (ValidateMeterResponse.data.invalid) {
-      return res.status(400).json(ValidateMeterResponse.data);
-    }
-    return res.status(200).json(ValidateMeterResponse.data);
-  } catch (error) {
-    // console.log(error);
-    res.status(500).json({
-      msg: "Unable to validate meter",
-    });
-  }
+  res.status(500).json({
+    msg: "An error occur.Please try again later",
+  });
 };
 
 const validateCableTv = async (req, res) => {
-  const { smart_card_number, cablename } = req.body;
-  if (!smart_card_number && !cablename)
-    return res.status(400).json({ msg: "All feilds are required" });
-  try {
-    const ValidateIUCResponse = await axios.get(
-      `https://www.gladtidingsdata.com/ajax/validate_iuc/?smart_card_number=${smart_card_number}&cablename=${cablename}`
-    );
-
-    return res.status(200).json(ValidateIUCResponse.data);
-  } catch (error) {
-    // console.log(error);
-    res.status(500).json({
-      msg: "An error occured.Please try again later",
-    });
-  }
+  res.status(500).json({
+    msg: "An error occur.Please try again later",
+  });
 };
 
 const buyElectricity = async (req, res) => {
-  const { meterId, meterNumber, amount, meterType } = req.body;
-  const { userId, userType } = req.user;
-  const amountToCharge = parseFloat(amount) + 50;
-  if (!meterId || !meterNumber || !amount || !meterType)
-    return res.status(400).json({ msg: "All feilds are required" });
-  const user = await User.findById(userId);
-  const { balance } = user;
-
-  if (amount < 1000)
-    return res.status(400).json({ msg: "minimum purchase is 1000" });
-  if (balance < amountToCharge || balance - amountToCharge < 0)
-    return res
-      .status(400)
-      .json({ msg: "Insufficient balance. Kindly fund your wallet" });
-  // Charging the user
-  await User.updateOne({ _id: userId }, { $inc: { balance: -amountToCharge } });
-
-  const response = await BUYELECTRICITY({ ...req.body });
-  const { status, token, msg } = response;
-  if (status) {
-    const receipt = await ELECETRICITY_RECEIPT({
-      package: "electricity token",
-      Status: "success",
-      token: token,
-      meter_number: meterNumber,
-      amountToCharge,
-      balance,
-      userId,
-    });
-    res.status(200).json({ msg: msg, receipt });
-  } else {
-    // return the charged amount
-    await User.updateOne(
-      { _id: userId },
-      { $inc: { balance: amountToCharge } }
-    );
-    res.status(500).json({ msg: msg || "Transaction failed" });
-  }
+  res.status(500).json({
+    msg: "An error occured.Please try again later",
+  });
 };
 const buyCableTv = async (req, res) => {
   return res.status(400).json({ msg: "Not available at the moment" });
-  res.send("buy cabletv");
 };
 module.exports = {
   buyAirtime,
